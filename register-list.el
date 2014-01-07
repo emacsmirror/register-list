b6b2e3b747c3a0bb2f0e255125bf1b220cfd9c38
bastien1 <bzg@gnu.org>
2014-01-07 14:40:19 +0100

    Update copyright years.

--- register-list.el
+++ register-list.el
@@ -1,6 +1,6 @@
 ;;; register-list.el ---  Interactively list/edit registers  -*- lexical-binding:t -*-
 ;;
-;; Copyright (C) 2011-2013  Free Software Foundation, Inc.
+;; Copyright (C) 2011-2014  Free Software Foundation, Inc.
 ;;
 ;; Filename: register-list.el
 ;; Author: Bastien Guerry <bzg AT altern DOT org>
